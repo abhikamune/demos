fc2f2952605b842ff88feed90a72394fc83db26c
Jesse Anderson <jesse@smokinghand.com>
2016-05-27 17:39:59 -0700

    Moved to CP 3.0. Fixes to get things compiling. Haven't run yet.

--- kstreamseventsim/src/main/java/eventsim/EventsimProcessorJob.java
+++ kstreamseventsim/src/main/java/eventsim/EventsimProcessorJob.java
@@ -3,29 +3,31 @@ package eventsim;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.Map;
+import java.util.Properties;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import javafx.util.Pair;
-
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
-import org.apache.kafka.streams.KafkaStreaming;
-import org.apache.kafka.streams.StreamingConfig;
-import org.apache.kafka.streams.examples.WallclockTimestampExtractor;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.processor.Processor;
 import org.apache.kafka.streams.processor.ProcessorContext;
-import org.apache.kafka.streams.processor.ProcessorDef;
+import org.apache.kafka.streams.processor.ProcessorSupplier;
 import org.apache.kafka.streams.processor.TopologyBuilder;
-import org.apache.kafka.streams.state.Entry;
-import org.apache.kafka.streams.state.InMemoryKeyValueStore;
+import org.apache.kafka.streams.processor.WallclockTimestampExtractor;
 import org.apache.kafka.streams.state.KeyValueIterator;
 import org.apache.kafka.streams.state.KeyValueStore;
+import org.apache.kafka.streams.state.internals.InMemoryKeyValueStoreSupplier;
 import org.apache.log4j.Logger;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.MinMaxPriorityQueue;
+
+import scala.Tuple2;
 
 public class EventsimProcessorJob {
 	static int windowsizems = 0;
@@ -33,31 +35,36 @@ public class EventsimProcessorJob {
 
 	final static Logger logger = Logger.getLogger(EventsimProcessorJob.class);
 
-	private static class EventsimProcessorDef implements ProcessorDef {
+	private static class EventsimProcessorDef implements ProcessorSupplier<String, String> {
 
 		@Override
-		public Processor<String, String> instance() {
+		public Processor<String, String> get() {
 			return new Processor<String, String>() {
 				private ProcessorContext context;
-				private KeyValueStore<String, String> kvStore;
+				private InMemoryKeyValueStoreSupplier<String, String> kvStore;
+				private KeyValueStore<String, String> stateStore;
+				
 				ObjectMapper mapper;
 
 				private HashMap<String, Long> artistToPlayCount;
 				private HashMap<String, Long> songToPlayCount;
 
-				private MinMaxPriorityQueue<Pair<String, Long>> artistToPlayCountQueue;
-				private MinMaxPriorityQueue<Pair<String, Long>> songToPlayCountQueue;
+				private MinMaxPriorityQueue<Tuple2<String, Long>> artistToPlayCountQueue;
+				private MinMaxPriorityQueue<Tuple2<String, Long>> songToPlayCountQueue;
 
 				private static final int TOP_N = 10;
 
 				private AtomicInteger currentGeneration = new AtomicInteger(0);
 
+				Serde<String> stringSerde = Serdes.String();
+				
 				@Override
 				public void init(ProcessorContext context) {
 					this.context = context;
 					this.context.schedule(windowsizems);
-					this.kvStore = new InMemoryKeyValueStore<String, String>(
-							"local-state", context);
+					this.kvStore = new InMemoryKeyValueStoreSupplier<String, String>(
+							"local-state", stringSerde, stringSerde);
+					this.stateStore = (KeyValueStore<String, String>) this.kvStore.get();
 					this.mapper = new ObjectMapper();
 
 					artistToPlayCount = new HashMap<String, Long>();
@@ -67,12 +74,12 @@ public class EventsimProcessorJob {
 				}
 
 				private void createQueues() {
-					Comparator<Pair<String, Long>> comparator = new Comparator<Pair<String, Long>>() {
+					Comparator<Tuple2<String, Long>> comparator = new Comparator<Tuple2<String, Long>>() {
 
 						@Override
-						public int compare(Pair<String, Long> o1,
-								Pair<String, Long> o2) {
-							return o1.getValue().compareTo(o2.getValue()) * -1;
+						public int compare(Tuple2<String, Long> o1,
+								Tuple2<String, Long> o2) {
+							return o1._2().compareTo(o2._2()) * -1;
 						}
 
 					};
@@ -86,11 +93,11 @@ public class EventsimProcessorJob {
 				@Override
 				public void process(String key, String value) {
 					String mapKey = getKeyName(value);
-					String oldValue = this.kvStore.get(mapKey);
+					String oldValue = stateStore.get(mapKey);
 
 					if (oldValue == null) {
 						// Swap k/v around as eventsim key is null
-						this.kvStore.put(mapKey, value);
+						stateStore.put(mapKey, value);
 					} else {
 						// TODO: Handle when k/v already there
 						// this.kvStore.put(key, oldValue + newValue);
@@ -103,21 +110,21 @@ public class EventsimProcessorJob {
 				public void punctuate(long streamTime) {
 					currentGeneration.incrementAndGet();
 
-					KeyValueIterator<String, String> iter = this.kvStore.all();
+					KeyValueIterator<String, String> iter = stateStore.all();
 
 					double totalDuration = 0;
 
 					long totalEntries = 0;
 
 					while (iter.hasNext()) {
-						Entry<String, String> entry = iter.next();
+						KeyValue<String, String> keyValue = iter.next();
 
 						totalEntries++;
 
-						if (entry.value() != null) {
+						if (keyValue.value != null) {
 							try {
-								JsonNode rootNode = mapper.readTree(entry
-										.value());
+								JsonNode rootNode = mapper.readTree(
+										keyValue.value);
 								/*
 								 * Example input:
 								 * {"ts":1442428043000,"userId":23545,
@@ -144,7 +151,7 @@ public class EventsimProcessorJob {
 
 								totalDuration += duration.asDouble();
 
-								if (checkDelete(entry.key())) {
+								if (checkDelete(keyValue.key)) {
 									iter.remove();
 								}
 							} catch (Exception e) {
@@ -213,7 +220,7 @@ public class EventsimProcessorJob {
 
 				private void processCounts(StringBuilder builder,
 						HashMap<String, Long> map,
-						MinMaxPriorityQueue<Pair<String, Long>> queue) {
+						MinMaxPriorityQueue<Tuple2<String, Long>> queue) {
 					Iterator<java.util.Map.Entry<String, Long>> iterator = map
 							.entrySet().iterator();
 
@@ -222,19 +229,19 @@ public class EventsimProcessorJob {
 					while (iterator.hasNext()) {
 						java.util.Map.Entry<String, Long> entry = iterator
 								.next();
-						queue.add(new Pair<String, Long>(entry.getKey(), entry
+						queue.add(new Tuple2<String, Long>(entry.getKey(), entry
 								.getValue()));
 					}
 
-					Iterator<Pair<String, Long>> queueIter = queue.iterator();
+					Iterator<Tuple2<String, Long>> queueIter = queue.iterator();
 
 					while (queueIter.hasNext()) {
-						Pair<String, Long> entry = queueIter.next();
+						Tuple2<String, Long> entry = queueIter.next();
 
-						builder.append("{\"name\" : \"").append(entry.getKey())
+						builder.append("{\"name\" : \"").append(entry._1())
 								.append("\",");
 
-						builder.append("\"count\" : ").append(entry.getValue())
+						builder.append("\"count\" : ").append(entry._2())
 								.append("}");
 
 						if (queueIter.hasNext()) {
@@ -259,7 +266,7 @@ public class EventsimProcessorJob {
 
 				@Override
 				public void close() {
-					this.kvStore.close();
+					this.kvStore.get().close();
 				}
 
 				private String getKeyName(String value) {
@@ -284,19 +291,15 @@ public class EventsimProcessorJob {
 		logger.info("Using windowsizems:" + windowsizems + " maxgenerations:"
 				+ maximumGenerations);
 
-		Map<String, Object> props = new HashMap<>();
-		props.put(StreamingConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-		props.put(StreamingConfig.KEY_SERIALIZER_CLASS_CONFIG,
+		Properties props = new Properties();
+		props.put(StreamsConfig.APPLICATION_ID_CONFIG, "event-sim-processor");
+		props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		props.put(StreamsConfig.KEY_SERDE_CLASS_CONFIG,
 				StringSerializer.class);
-		props.put(StreamingConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+		props.put(StreamsConfig.VALUE_SERDE_CLASS_CONFIG,
 				StringSerializer.class);
-		props.put(StreamingConfig.KEY_DESERIALIZER_CLASS_CONFIG,
-				StringDeserializer.class);
-		props.put(StreamingConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
-				StringDeserializer.class);
-		props.put(StreamingConfig.TIMESTAMP_EXTRACTOR_CLASS_CONFIG,
+		props.put(StreamsConfig.TIMESTAMP_EXTRACTOR_CLASS_CONFIG,
 				WallclockTimestampExtractor.class);
-		StreamingConfig config = new StreamingConfig(props);
 
 		TopologyBuilder builder = new TopologyBuilder();
 
@@ -308,7 +311,7 @@ public class EventsimProcessorJob {
 		builder.addSink("SINK", "eventsimstream", new StringSerializer(),
 				new StringSerializer(), "PROCESS");
 
-		KafkaStreaming streaming = new KafkaStreaming(builder, config);
+		KafkaStreams streaming = new KafkaStreams(builder, props);
 		streaming.start();
 	}
 }
